0fe416fd0617da0c653461e919603c163eb218f2
SuniRein <sunirein@qq.com>
2025-12-07 22:04:46 +0800

    feat(server): add tracing

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,18 +2,21 @@ use axum::{Router, routing::get};
 use sqlx::postgres::PgPool;
 use std::env;
 use tokio::net::TcpListener;
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPool::connect(&database_url).await?;
-    println!("Connected to the database");
+    info!("Connected to the database");
 
     sqlx::migrate!().run(&pool).await?;
 
     let addr = env::var("SERVER_ADDR").unwrap_or_else(|_| "127.0.0.1:9000".to_string());
     let listener = TcpListener::bind(addr).await?;
-    println!("Server running on {}", listener.local_addr().unwrap());
+    info!("Server running on {}", listener.local_addr().unwrap());
 
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 

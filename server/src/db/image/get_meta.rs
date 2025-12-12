5e072056d7f8036946a39db2ee36fbd62b795883
SuniRein <sunirein@qq.com>
2025-12-12 23:32:34 +0800

    feat(server/api): return tags when get image metas

--- server/src/db/image/get_meta.rs
+++ server/src/db/image/get_meta.rs
@@ -18,6 +18,8 @@ pub struct ImageMeta {
 
     is_public: bool,
 
+    tags: Vec<String>,
+
     created_at: NaiveDateTime,
     updated_at: NaiveDateTime,
 }
@@ -29,12 +31,25 @@ pub async fn get_all_image_metas(
 ) -> sqlx::Result<Vec<ImageMeta>> {
     sqlx::query_as!(
         ImageMeta,
-        "SELECT
-          id, owner_id, category_id, size_bytes, width, height, mime_type, exif,
-          is_public, created_at, updated_at
-         FROM image
-         ORDER BY id
-         LIMIT $1 OFFSET $2",
+        r#"
+         WITH img AS (
+           SELECT
+             id, owner_id, category_id, size_bytes, width, height, mime_type, exif,
+             is_public, created_at, updated_at
+           FROM image i
+           ORDER BY id
+           LIMIT $1 OFFSET $2
+         )
+         SELECT img.*, COALESCE(tl.tags, '{}'::text[]) AS "tags!"
+         FROM img
+         LEFT JOIN LATERAL (
+           SELECT array_agg(t.name ORDER BY t.name) AS tags
+           FROM image_tag it
+           JOIN tag t ON it.tag_id = t.id
+           WHERE it.image_id = img.id
+         ) tl on TRUE
+         ORDER BY img.id
+        "#,
         pagination.limit,
         pagination.offset,
     )
@@ -47,11 +62,20 @@ pub async fn get_all_image_metas(
 pub async fn get_image_meta_by_id(pool: &PgPool, id: i32) -> sqlx::Result<Option<ImageMeta>> {
     sqlx::query_as!(
         ImageMeta,
-        "SELECT
+        r#"
+         SELECT
           id, owner_id, category_id, size_bytes, width, height, mime_type, exif,
-          is_public, created_at, updated_at
-         FROM image
-         WHERE id = $1",
+          is_public, created_at, updated_at,
+          COALESCE(tl.tags, '{}'::text[]) AS "tags!"
+         FROM image i
+         LEFT JOIN LATERAL (
+           SELECT array_agg(t.name ORDER BY t.name) AS tags
+           FROM image_tag it
+           JOIN tag t ON it.tag_id = t.id
+           WHERE it.image_id = i.id
+         ) tl on TRUE
+         WHERE id = $1
+        "#,
         id
     )
     .fetch_optional(pool)
@@ -67,13 +91,26 @@ pub async fn get_image_metas_by_owner(
 ) -> sqlx::Result<Vec<ImageMeta>> {
     sqlx::query_as!(
         ImageMeta,
-        "SELECT
-          id, owner_id, category_id, size_bytes, width, height, mime_type, exif,
-          is_public, created_at, updated_at
-         FROM image
-         WHERE owner_id = $1
-         ORDER BY id
-         LIMIT $2 OFFSET $3",
+        r#"
+         WITH img AS (
+           SELECT
+             id, owner_id, category_id, size_bytes, width, height, mime_type, exif,
+             is_public, created_at, updated_at
+           FROM image i
+           WHERE owner_id = $1
+           ORDER BY id
+           LIMIT $2 OFFSET $3
+         )
+         SELECT img.*, COALESCE(tl.tags, '{}'::text[]) AS "tags!"
+         FROM img
+         LEFT JOIN LATERAL (
+           SELECT array_agg(t.name ORDER BY t.name) AS tags
+           FROM image_tag it
+           JOIN tag t ON it.tag_id = t.id
+           WHERE it.image_id = img.id
+         ) tl on TRUE
+         ORDER BY img.id
+        "#,
         owner_id,
         pagination.limit,
         pagination.offset,

8dc83b87b780d71e527264577ce6cf1fa2f154ac
SuniRein <sunirein@qq.com>
2025-12-30 21:06:41 +0800

    feat(server/image): set max image size limit to 20MB for uploading

--- server/src/api/images/router.rs
+++ server/src/api/images/router.rs
@@ -1,7 +1,12 @@
+use axum::extract::DefaultBodyLimit;
 use sqlx::PgPool;
 use utoipa_axum::{router::OpenApiRouter, routes};
 
 pub fn create_router() -> OpenApiRouter<PgPool> {
+    let upload_router = OpenApiRouter::new()
+        .routes(routes!(super::upload::upload_raw_image))
+        .layer(DefaultBodyLimit::max(20 * 1024 * 1024));
+
     OpenApiRouter::new()
         .routes(routes!(super::get_stats::get_image_count))
         .routes(routes!(super::get_meta::get_image_meta))
@@ -10,7 +15,7 @@ pub fn create_router() -> OpenApiRouter<PgPool> {
         .routes(routes!(super::get_raw::get_image_signed))
         .routes(routes!(super::get_thumbnail::get_thumbnail))
         .routes(routes!(super::get_thumbnail::get_thumbnail_signed))
-        .routes(routes!(super::upload::upload_raw_image))
+        .merge(upload_router)
         .routes(routes!(
             super::tag::get_image_tags,
             super::tag::set_image_tags,
